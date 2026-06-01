07ad09806ecc2464f006da77bc22d3df7177fb19
Evalyn Alviar <ealviar.12805@gmail.com>
2026-06-01 19:14:03 +0000

    grab spans, add event listener and highlight all lines for clicked actor

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,2 +1,27 @@
-alert("Hello from dom-play.js!");
+// grab spans in #play
+const spans = document.querySelectorAll("#play span.actor");
+
+// add event listeners to each actor span
+for (const mySpan of spans) {
+    
+    const actor = mySpan.dataset.actor;
+
+    mySpan.addEventListener("click", function (){
+        highlightActor(actor);
+    });
+}
+
+// hightlight all lines for the clicked actor
+function highlightActor(actorName) {
+
+    for (const s of spans) {
+        if (s.dataset.actor === actorName) {
+            s.style.backgroundColor = "yellow";
+            s.style.fontWeight = "bold";
+        } else {
+            s.style.backgroundColor = "white";
+            s.style.fontWeight = "normal";
+        }
+    }
+}
       
